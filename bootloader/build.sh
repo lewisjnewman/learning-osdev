bf40e42e25dd762cac2ac1130a2d513b43acdbec
Lewis Newman <lewis.newman.uk@gmail.com>
2020-08-29 21:28:00 +0100

    Reformatted build.sh to make it nicer to read

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -1,2 +1,13 @@
+
 nasm ./asm/boot.asm -f elf32 -o boot.o
-clang --target=i686-pc-none-elf -march=i686 -ffreestanding -fno-builtin -nostdlib -nostdinc -T linker.ld src/*.c *.o -o bootloader.bin
+
+clang \
+    --target=i686-pc-none-elf -march=i686 \
+    -ffreestanding -fno-builtin -nostdlib \
+    -Wall -Werror \
+     -T linker.ld \
+     -I ./include/ \
+     -o bootloader.bin \
+     ./src/*.c ./*.o
+
+rm ./*.o
