9018733a93f6361aaf2872a45d73ae24ca011732
Lewis Newman <lewis.newman.uk@gmail.com>
2020-08-30 00:19:42 +0100

    Tidying up main

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -2,19 +2,10 @@
 #include <mem_locations.h>
 #include <vga_io.h>
 
-const char* hello = "Hello From The C Bootloader";
-
 void boot_main(){
     
        clear_screen();
-       puts(hello);
-
-/*
-       vga_char filler;
-       filler.character = 'x';
-       filler.colour_bg = BLUE;
-       filler.colour_fg = WHITE;
-
-       fill_screen(filler);
-*/
+ 
+ 
+       b_puts("Hello From The C Bootloader\n");
 }
