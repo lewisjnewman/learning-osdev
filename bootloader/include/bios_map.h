4283403aeb7f8180bb11de162b6f2f628c69d7fc
Lewis Newman <lewis.newman.uk@gmail.com>
2020-08-30 12:35:16 +0100

    Adjusted to the 24byte version of the struct, also added an array of strings for help in printing out the memory map

--- bootloader/include/bios_map.h
+++ bootloader/include/bios_map.h
@@ -7,7 +7,8 @@
 typedef struct __attribute__((__packed__)){
     uint64_t base_address;
     uint64_t map_size;
-    uint32_t type;    
+    uint32_t type;
+    uint32_t acpi3;
 } bios_memmap;
 
 //types of maps
@@ -15,5 +16,14 @@ typedef struct __attribute__((__packed__)){
 #define BIOS_MAP_RESERVED       0x02
 #define BIOS_MAP_ACPI_RECLAIM   0x03
 #define BIOS_MAP_ACPI_NVS       0x04
+
+char* bios_mem_map_strings[] = {
+    "ERR",
+    "MEM_FREE",
+    "MEM_RESERVED",
+    "MEM_ACPI_RECLAIM",
+    "MEM_ACPI_NVS",
+    "UNKNOWN",
+};
 
 #endif
