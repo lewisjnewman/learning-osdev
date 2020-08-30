dddc8d664fc4b3730f4595cd7e0fb77411d8facf
Lewis Newman <lewis.newman.uk@gmail.com>
2020-08-30 01:27:19 +0100

    Fixed integer to hexstring conversions

--- bootloader/src/conversions.c
+++ bootloader/src/conversions.c
@@ -102,15 +102,29 @@ void uint64_to_str(uint64_t val, char* str){
 char hex_digit_table[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 void bits32_to_hex(uint32_t val, char* str){
-    for(uint32_t i = 0; i < 2*sizeof(uint32_t); i++){
-        *str++ = hex_digit_table[val&0x000000F];
+    for(uint32_t i = 0; i < sizeof(uint32_t); i++){
+        *str++ = hex_digit_table[val&0xF];
         val = val >> 4;
+
+        *str++ = hex_digit_table[val&0xF];
+        val = val >> 4;
+
+        char tmp = *(str-1);
+        *(str-1) = *(str-2);
+        *(str-2) = tmp;
     }
 }
 
 void bits64_to_hex(uint64_t val, char* str){
-    for(uint32_t i = 0; i < 2*sizeof(uint64_t); i++){
-        *str++ = hex_digit_table[val&0x000000F];
+    for(uint32_t i = 0; i < sizeof(uint64_t); i++){
+        *str++ = hex_digit_table[val&0xF];
         val = val >> 4;
+
+        *str++ = hex_digit_table[val&0xF];
+        val = val >> 4;
+
+        char tmp = *(str-1);
+        *(str-1) = *(str-2);
+        *(str-2) = tmp;
     }
 }
