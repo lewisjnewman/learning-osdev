59963e32519b919661928f51160e42a0b6ddd55c
Lewis Newman <lewis.newman.uk@gmail.com>
2020-08-30 22:55:13 +0100

    Improving the build script to list files in src subdirectories

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -1,5 +1,7 @@
 
 nasm ./asm/boot.asm -f elf32 -o boot.o
+nasm ./asm/io.asm -f elf32 -o io.o
+
 
 clang \
     --target=i686-pc-none-elf -march=i686 \
@@ -8,6 +10,7 @@ clang \
      -T linker.ld \
      -I ./include/ \
      -o bootloader.bin \
-     ./src/*.c ./*.o
+     $(find ./src -type f -name '*.c' -print) \
+     $(find . -type f -name '*.o' -print)
 
 rm ./*.o
